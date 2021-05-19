9480afb9192858dcb49bd696adbff22327f7c03f
michdev7 <michele.bev@outlook.com>
2021-05-19 11:50:36 +0200

    Spsotato file stylealex.css nella cartella style

--- head.php
+++ head.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap" rel="stylesheet">
     <link href="./style/style.css" rel="stylesheet" type="text/css"/>
+    <link href="./style/stylealex.css" rel="stylesheet" type="text/css"/>
     <link rel="apple-touch-icon" sizes="180x180" href="./img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon/favicon-16x16.png">
