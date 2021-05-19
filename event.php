05f6993c980ca7ef1f32a82214809a85127c30fa
Alex Dediu <alex.dediu@outlook.it>
2021-05-20 01:47:55 +0200

    aggiunta mappa evento

--- event.php
+++ event.php
@@ -6,7 +6,6 @@
 <?php require("./template-parts/static/front/header.php")?>
 <div class="hero"> 
     <div class="hero-content">
-        
     <div class="text-hero">
     <h2>Tour Artista</h2>
     <h4>24 giu 2022</h4>
@@ -17,6 +16,16 @@
     </div>
     </div>
 </div>
+<h3><b><u>Mappa Evento</u></b></h3>
+
+
+</div>
+<div class="map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11190.555740991489!2d9.12007382938012!3d45.47707686397451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786c193fa23f19d%3A0x9c7d30c7aeff312!2sStadio%20San%20Siro!5e0!3m2!1sit!2sit!4v1621464654299!5m2!1sit!2sit" width="1000px" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+</div>
+
+
 </body>
 <!--
 <script>
