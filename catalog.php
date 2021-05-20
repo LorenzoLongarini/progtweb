1a4e8d93291c8c6fb45acdafca4f731b60b1a26d
michdev7 <michele.bev@outlook.com>
2021-05-20 17:48:37 +0200

    Modifiche generali

--- catalog.php
+++ catalog.php
@@ -5,67 +5,71 @@
 <body>
 <?php require("./template-parts/static/front/header.php")?>
 
-<div class="event-list">
-    <h1><u>CATALOGO</u></h1>
-<div class="event">
-<div class="img-event">
-    <h3>Immagine</h3>
-</div>
-<div>
-    <h2>Tour Artista</h2>
-    <h4>17 giu 2022</h4>
-    <h4>Milano San Siro</h4>
-</div>
-<button class="btn default-btn">VEDI EVENTO</button>
+<main id="page-content">
+        <div class="container block">
+        <div class="event-list">
+        <h1><u>CATALOGO</u></h1>
+    <div class="event">
+    <div class="img-event">
+        <h3>Immagine</h3>
+    </div>
+    <div>
+        <h2>Tour Artista</h2>
+        <h4>17 giu 2022</h4>
+        <h4>Milano San Siro</h4>
+    </div>
+    <button class="btn default-btn">VEDI EVENTO</button>
 
-</div>
-<div class="event">
-<div class="img-event">
-    <h3>Immagine</h3>
-</div>
-<div>
-    <h2>Tour Artista</h2>
-    <h4>17 giu 2022</h4>
-    <h4>Milano San Siro</h4>
-</div>
-<button class="btn default-btn">VEDI EVENTO</button>
+    </div>
+    <div class="event">
+    <div class="img-event">
+        <h3>Immagine</h3>
+    </div>
+    <div>
+        <h2>Tour Artista</h2>
+        <h4>17 giu 2022</h4>
+        <h4>Milano San Siro</h4>
+    </div>
+    <button class="btn default-btn">VEDI EVENTO</button>
 
-</div>
-<div class="event">
-<div class="img-event">
-    <h3>Immagine</h3>
-</div>
-<div>
-    <h2>Tour Artista</h2>
-    <h4>17 giu 2022</h4>
-    <h4>Milano San Siro</h4>
-</div>
-<button class="btn default-btn">VEDI EVENTO</button>
+    </div>
+    <div class="event">
+    <div class="img-event">
+        <h3>Immagine</h3>
+    </div>
+    <div>
+        <h2>Tour Artista</h2>
+        <h4>17 giu 2022</h4>
+        <h4>Milano San Siro</h4>
+    </div>
+    <button class="btn default-btn">VEDI EVENTO</button>
 
-</div>
-<div class="event">
-<div class="img-event">
-    <h3>Immagine</h3>
-</div>
-<div>
-    <h2>Tour Artista</h2>
-    <h4>17 giu 2022</h4>
-    <h4>Milano San Siro</h4>
-</div>
-<button class="btn default-btn">VEDI EVENTO</button>
-</div>
-<div class="event">
-<div class="img-event">
-    <h3>Immagine</h3>
-</div>
-<div>
-    <h2>Tour Artista</h2>
-    <h4>17 giu 2022</h4>
-    <h4>Milano San Siro</h4>
-</div>
-<button class="btn default-btn">VEDI EVENTO</button>
-</div>
-</div>
+    </div>
+    <div class="event">
+    <div class="img-event">
+        <h3>Immagine</h3>
+    </div>
+    <div>
+        <h2>Tour Artista</h2>
+        <h4>17 giu 2022</h4>
+        <h4>Milano San Siro</h4>
+    </div>
+    <button class="btn default-btn">VEDI EVENTO</button>
+    </div>
+    <div class="event">
+    <div class="img-event">
+        <h3>Immagine</h3>
+    </div>
+    <div>
+        <h2>Tour Artista</h2>
+        <h4>17 giu 2022</h4>
+        <h4>Milano San Siro</h4>
+    </div>
+    <button class="btn default-btn">VEDI EVENTO</button>
+    </div>
+    </div>
 
+    </div>
+    </main>
 </body>
 </html>
