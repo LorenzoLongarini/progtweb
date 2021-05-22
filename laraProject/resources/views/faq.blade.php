1f9f22530c46651cb9832afbdb8a67ed99ff19f6
Alex Dediu <alex.dediu@outlook.it>
2021-05-22 19:59:10 +0200

    modifiche

--- laraProject/resources/views/faq.blade.php
+++ laraProject/resources/views/faq.blade.php
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-@extends(/layouts.public)
-=======
 @extends('layouts.public')
 
 @section('faq-content')
-<div class="hero flex-center" style="background-image: linear-gradient(0deg, rgba(0,0,0,.8), rgba(0,0,0,0.6)),url('{{asset ('/images/faq-cover.jpg')}}');background-position:center center;background-size:cover"> 
+<div class="hero flex-center"> 
     <div class="container flex-rows">
         <div class="content-hero">
            <h2>FAQ - Frequently asked questions</h2>
@@ -12,5 +9,5 @@
         </div>
     </div>
 </div>
+
 @endsection
->>>>>>> 8e043e40a5caca3e6d583ece028248ee28962740
