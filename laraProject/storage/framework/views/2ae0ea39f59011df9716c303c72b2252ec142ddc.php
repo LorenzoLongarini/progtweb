decacf0fd98bbac50896b7d07b5eafcc222b486b
Alex Dediu <alex.dediu@outlook.it>
2021-05-22 12:05:18 +0200

    #michdonttouchmycss
    bo,sono obbligato a farlo

---
+++ laraProject/storage/framework/views/2ae0ea39f59011df9716c303c72b2252ec142ddc.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="container flex-columns flex-center">
+          <div class="block">
+            <img src="<?php echo e(asset ('/images/logos/eticket_logo.svg')); ?>" width="220px" height="auto"/>
+          </div>
+          <div class="block">
+            <ul class="footer-links">
+              <li><a href="#">FAQ</a></li>
+              <li><a href="chi-siamo.php">Contatti</a></li>
+              <li><a href="privacy.php">Privacy policy</a></li>
+              <li><a href="#">Termini d'uso</a></li>
+              <li><a href="#">Lavora con noi</a></li>
+            </ul>
+          </div>
+        <div class="site-info">
+            <h4>&copy; <?php echo date('Y')?> EasyTicket S.r.l.</h4>
+        </div>
+    </div>
+</footer><?php /**PATH /Applications/MAMP/htdocs/progettoweb21/laraProject/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
