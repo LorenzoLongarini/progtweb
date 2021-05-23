41145d78387034bed5b3da06583b34a21d94d9f7
Alex Dediu <alex.dediu@outlook.it>
2021-05-24 01:35:52 +0200

    Pagina User-2 da rivedere
    ora sono cotto.
    la rivedo domani.
    pace e amore.

--- user-2.php
+++ user-2.php
@@ -2,26 +2,28 @@
 <html lang="en">
 <?php require ("head.php")?>
 <body>
-    <?php require ("./template-parts/static/front/header.php")?>
+    <?php require ("./template-parts/static/front/header-users.php")?>
     <main id="page-content">
-    <div class="container flex-rows">
+    <div class="container flex-rows" >
         <aside id="user-nav">
             <ul>
                 <li class="user-nav-link">
-                    <button onclick=""id="dashboard" class="user-nav-btn active">Dashboard</button>
+                    <button onclick="activeUtente()"id="utente" class="user-nav-btn ">Dati personali</button>
                     <h3></h3>
                 </li>
                 <li class="user-nav-link">
                     <button onclick="activeStorico()" id="storico" class="user-nav-btn  ">Storico acquisti</button>
                 </li>
+                <!--
                 <li class="user-nav-link">
-                    <button  onclick="activeSettings()"id="impostazioni-utente" class="user-nav-btn ">Impostazioni utente</button>
+                    <button  onclick="activeSettings()"id="impostazioni-utente" class="user-nav-btn "></button>
                 </li>
+                -->
             </ul>
         </aside>
         <div class="user-nav-separator"></div>
 
-        <section class="#" style="width: fit-content" id="user-section">
+        <section class="#" style="width:fit-content;display:none" id="user-section" >
             <p>
             <input type="text" value="" maxlength="20" rows="1" placeholder="Nome">
             <input type="text" value="" maxlength="20" rows="1" placeholder="Cognome">
@@ -38,41 +40,41 @@
             <button>Annulla</button>
             </p>
         </section>
+        
+        <div class="storico" id="storico-user">
+            <div class="acquisto">acquisto</div>
+            <div class="acquisto">acquisto</div>
+            <div class="acquisto">acquisto</div>
+            <div class="acquisto">acquisto</div>
+
+        </div>
     </div>
 </main>
 </body>
 <script>
-/*
-    function activeDashboard(){
-        var elem=document.getElementById("dashboard");
+
+    function activeUtente(){
+        var elem=document.getElementById("utente");
        var elem1=document.getElementById("storico");
-       var elem2=document.getElementById("impostazioni-utente");
-        var elem3=document.getElementById("user-section");
+       var storico=document.getElementById("storico-user");
+       var utente=document.getElementById("user-section");
 
         if(elem.classList.contains("active"))
-            elem3.style.display="none"
+            utente.style.display="none";
         else
-            elem3.style.display="block";
-
-
-
+            utente.style.display="block";
+            storico.style.display="none";
+       
         if(elem.classList.contains("active"))
-            elem3.style.display="block";
-           
-           
+            elem.classList.remove("active");
 
         else 
             elem.classList.add("active");
             elem1.classList.remove("active");
-            elem2.classList.remove("active");
             
-           
-           
+
+      
     }
-
-*/
-
-
 
 
 </script>
@@ -80,33 +82,29 @@
 
 function activeStorico(){
         var elem=document.getElementById("storico");
-       var elem1=document.getElementById("dashboard");
-       var elem2=document.getElementById("impostazioni-utente");
+       var elem1=document.getElementById("utente");
+       var storico=document.getElementById("storico-user");
+       var utente=document.getElementById("user-section");
+       
+
+        if(elem.classList.contains("active"))
+            storico.style.display="none";
+        else
+            storico.style.display="block";
+            utente.style.display="none";
+           
+
 
         if(elem.classList.contains("active"))
            elem.classList.remove("active");
         else 
             elem.classList.add("active");
             elem1.classList.remove("active");
-            elem2.classList.remove("active");
+            
            
     }
-*/
-</script>
-<script>
-    
-function activeSettings(){
-        var elem=document.getElementById("impostazioni-utente");
-       var elem1=document.getElementById("dashboard");
-       var elem2=document.getElementById("storico");
-
-        if(elem.classList.contains("active"))
-           elem.classList.remove("active");
-        else 
-            elem.classList.add("active");
-            elem1.classList.remove("active");
-            elem2.classList.remove("active");
-}
 
 </script>
+
+
 </html>
