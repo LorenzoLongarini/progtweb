16dc2b44a63248b1eed739a39a5659d811f7860e
michdev7 <michele.bev@outlook.com>
2021-05-23 23:41:36 +0200

    Modificato migration

--- laraProject/database/migrations/2021_05_23_204733_create_partecipazioni_table.php
+++ laraProject/database/migrations/2021_05_23_204733_create_partecipazioni_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTablePartecipazioni extends Migration
+class CreatePartecipazioniTable extends Migration
 {
     /**
      * Run the migrations.
