6a4a9ab9b5c50ed8a60f900c67759b27cb537ac2
michdev7 <michele.bev@outlook.com>
2021-05-27 02:53:49 +0200

    <div> C r'p'gliamm tutt chill ch'è u nuostr </div>

--- laraProject/app/Http/Controllers/PublicController.php
+++ laraProject/app/Http/Controllers/PublicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Faq;
+use App\Models\Evento;
 
 class PublicController extends Controller
 {
@@ -29,39 +30,25 @@ class PublicController extends Controller
         return view('pages.home');
     }
 
-    public function registraUser(){
-        return view('insert.registerUser');
-    }
-
-    public function salvaUser(UserRequest $request){
-        $user = new User;
-        $user->username = $request->username;
-       // $user->password = Hash::make($request->password);
-        $user->nome=$request->nome;
-        $user->cognome=$request->cognome;
-        $user->ragioneSociale=$request->ragioneSociale;
-        $user->ivaFiscale=$request->ivaFiscale;
-        $user->dataNascita=$request->dataNascita;
-        $user->email=$request->email;
-        $user->telefono=$request->telefono;
-        $user->via=$request->via;
-        $user->cap=$request->cap;
-        $user->città=$request->città;
-        $user->livello='cliente';
-        $user->save();
-
-        
-    }
-
     /**
      * Restituisce un array di faq prelevate dal db tramite Eloquent ORM
      */
     public function showSiteFaqs(){
-        $faqs = new Faq;
-        return view('pages.faq')->with('faqs', $faqs::all());
+        return view('pages.faq')->with('faqs', Faq::all());
+    }
+
+    public function showCatalogo(){
+        return view('pages.catalogo')->with('events', Evento::all());
     }
 
     public function searchForFilters(Request $request){
-        
+        $eventiFiltrati = Evento::query()
+                        ->where('titolo', 'LIKE' , '%' . $request->titolo . '%')
+                        ->where('artista', 'LIKE', '%' . $request->artista . '%')
+                        ->where('descrizione', 'LIKE', '%' . $request->descrizione . '%')
+                        ->where('regione', 'LIKE', '%' . $request->regione . '%')
+                        ->where('data', '>=', $request->data)->get();
+                        
+        return view('pages.catalogo')->with('events', $eventiFiltrati);
     }
 }
--- laraProject/resources/views/pages/catalogo.blade.php
+++ laraProject/resources/views/pages/catalogo.blade.php
@@ -10,135 +10,27 @@
 
     <div class="container block">
         @include('Insert.filterEvent')
-        <div class="event-list">
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-            <div class="event">
-                <div class="event-left">
-                    <div class="img-event">
-                        <h3>Immagine</h3>
-                    </div>
-                    <div class="event-info flex-columns">
-                        <h2>Tour Artista</h2>
-                        <h4>17 giu 2022</h4>
-                        <h4>Milano San Siro</h4>
-                    </div>
-                </div>
-                <div class="event-right">
-                    <h4 class="price">€ 32,90</h4>
-                    <button class="btn default-btn">VEDI EVENTO</button>
-                </div>
-            </div>
-        </div>
+
+        @isset($events)
+            <div class="event-list">
+                @foreach($events as $event)
+                    <div class="event">
+                        <div class="event-left">
+                            <div class="img-event">
+                                @include('helpers.img-event', ['imgName' => $event->imgName])
+                            </div>
+                            <div class="event-info flex-columns">
+                                <h2>{{ $event->titolo }}</h2>
+                                <h4>{{ $event->data }}</h4>
+                                <h4>{{ $event->luogo }}, {{ $event ->regione }}</h4>
+                            </div>
+                        </div>
+                        <div class="event-right">
+                            <h4 class="price">{{ $event->prezzo }}</h4>
+                            <button class="btn default-btn">VEDI EVENTO</button>
+                        </div>
+                    </div>           
+            @endforeach
+        @endisset
     </div>
 @endsection
