5dc1d73f82248bf74813e58d029db7507027315c
GiacchiD <s1089234@studenti.univpm.it>
2021-05-29 18:33:24 +0200

    modifyFaq

--- laraProject/resources/views/inserts/modifyFaq.blade.php
+++ laraProject/resources/views/inserts/modifyFaq.blade.php
@@ -1,10 +1,10 @@
 <div class="form-blank">
-    {{  Form::open(array('route' => ['modificaFaq.store', $faq->faqID;] , 'files' => true, 'method'->'PATCH'))  }}
+    {{  Form::open(array('route' => ['modificaFaq.store', $faq->faqId] , 'files' => true, 'method'->'PATCH'))  }}
    
     <h2>Modifica FAQ</h2>
         <div class="wrap-input">
         {{  Form::label('domanda', 'Domanda')}}
-        {{  Form::textarea('domanda', $faq->domanda)}}
+        {{  Form::textarea('domanda', {{$faq->domanda}})}}
         @if ($errors->first('domanda'))
                 <ul>
                     @foreach ($errors->get('domanda') as $message)
@@ -16,7 +16,7 @@
 
         <div class="wrap-input">
         {{  Form::label('risposta',  'Risposta')}}
-        {{  Form::textarea('risposta', $faq->risposta)}}
+        {{  Form::textarea('risposta', {{$faq->risposta}})}}
         @if ($errors->first('risposta'))
                 <ul>
                     @foreach ($errors->get('risposta') as $message)
