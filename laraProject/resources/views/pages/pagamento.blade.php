c40701d71a4a60707572d36aa9bff8897c824a21
LorenzoLongarini <s1089145@studenti.univpm.it>
2021-06-07 11:59:28 +0200

    cambiamenti

--- laraProject/resources/views/pages/pagamento.blade.php
+++ laraProject/resources/views/pages/pagamento.blade.php
@@ -31,14 +31,25 @@
 </div>
 <div class = "pagamento-line"></div>
     <div class = "row-direction">
-    
+    {{--@if(($pagamento->sconto)=== 0)
+    <div class = "dim-prezzo-mod">{{$pagamento->prezzo}} €</div>
+    @else
     <div class = "dim-prezzo-mod">{{$pagamento->prezzo - ($pagamento->prezzo / (100/$pagamento->sconto))}} €</div>
+    @endif --}}
     </div>
     <div class = "row-direction">
     <h3 class="notice quant-dim">
-    Applicato sconto last-minute del {{$pagamento->sconto}}%
+    @if(($pagamento->sconto)=== 0)
+    Nessuno sconto applicato
+    @else
+            Applicato sconto last-minute del {{$pagamento->sconto}}%
+    @endif
             </h3>
+            @if(($pagamento->sconto)=== 0)
+            <div class = "dim-prezzo-mod2">{{$pagamento->prezzo}} €</div>
+            @else
             <div class = "dim-prezzo-mod2">{{$pagamento->prezzo - ($pagamento->prezzo / (100/$pagamento->sconto))}} €</div>
+            @endif  
     </div>
     <div class = "pagamento-line"></div>
         <div class = "row-direction">
@@ -48,6 +59,38 @@
 </div>
     <div class = "pagamento-line-v"></div>
     <div>
+    <div class = "row-direction">
+        <img src="./img/credit-card.svg" style="width:8%" hspace="20">
+        <div style="margin-top: 8px"> <span style="font-weight:bolder"> Metodo di pagamento</div>
+    </div>
+            <br>
+            <div>
+            <label class="esempio-form" for="nome-tit">Nome Titolare</label><br>
+            <input class="esempio-form" id = "nome-tit" type="text"  value="" maxlength="20" rows="1" style="width: 70%">
+            </div>
+            <br>
+            <label class="esempio-form" for="cognome-tit">Cognome Titolare</label><br>
+            <input class="esempio-form" id = "cognome-tit" type="text" value="" maxlength="20" rows="1" style="width: 70%">
+            </br>
+            <br>
+            <label class="esempio-form" for="codice">Codice</label><br>
+            <input class="esempio-form" id = "codice" type="text" value="" maxlength="20" rows="1" style="width: 70%" >
+            <br>
+            <label class="esempio-form">Seleziona Metodo di pagamento</label><br>
+            <input class="esempio-form" id = "carta" type="radio">
+            <input class="esempio-form" id = "carta" type="radio">
+            <input class="esempio-form" id = "carta" type="radio">
+            <br>
+            <label class="esempio-form" for="card-number-area">Numero Carta</label><br>
+            <input class = "esempio-form" type="text" id="card-number-area" maxlength="19" rows="1" size="19" placeholder="XXXX-XXXX-XXXX-XXXX"></input>
+            <br>
+            <div class = "row-direction">
+            <label class="esempio-form" for="card-expiration-date">CVV</label>
+            <label class="esempio-form" for="card-security-code" style="margin-left:80px">Scadenza</label>
+            </div>  
+            <div class = "row-direction">
+            <input class = "esempio-form" type="text" id="card-expiration-date" maxlength="5" size ="2" rows="1" placeholder="XX/XX"></input>
+            <input style = "margin-left:0px" class = "esempio-form" type="text" id="card-security-code" maxlength="3" size="2" rows="1" placeholder="XXX"></input>
     @section('form')
     @include('inserts.pagamento-form')
 @endsection
