361104041947ca22129202e332b70e1437b1549f
michdev7 <michele.bev@outlook.com>
2021-06-09 11:34:13 +0200

    Statistiche Livello3

--- laraProject/app/Models/Statistiche.php
+++ laraProject/app/Models/Statistiche.php
@@ -13,7 +13,7 @@ class Statistiche{
         $biglietti = Biglietto::bigliettiByOrg($orgId);
 
         foreach($biglietti as $biglietto){
-            $guadagnoTotale += $biglietto->prezzoAcquisto;
+            $guadagnoTotale += $biglietti->prezzoAcquisto;
         }
         
         return $guadagnoTotale;
@@ -22,5 +22,20 @@ class Statistiche{
     public static function bigliettiVendutiByOrg($orgId){
         $bigliettiVenduti = Biglietto::bigliettiByOrg($orgId);
         return count($bigliettiVenduti);
+    }
+
+    public static function bigliettiVendutiByEvento($eventoId){
+        return Evento::where('eventoId', $eventoId)->get(['bigliettiVenduti']);
+    }
+
+    public static function incassoEvento($eventoId){
+        $incasso = 0;
+        $biglietti = Biglietto::where('eventoId', $eventoId)->get(['prezzoAcquisto']);
+
+        foreach($biglietti as $biglietto){
+            $incasso += $biglietto->prezzoAcquisto;
+        }
+
+        return $incasso;
     }
 }
