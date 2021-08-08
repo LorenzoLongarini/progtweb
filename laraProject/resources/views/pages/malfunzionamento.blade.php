18bf4e0a0ed6295556876ff1408855fa10dfadbf
LorenzoLongarini <s1089145@studenti.univpm.it>
2021-08-08 19:33:00 +0200

    aggiustamenti

--- laraProject/resources/views/pages/malfunzionamento.blade.php
+++ laraProject/resources/views/pages/malfunzionamento.blade.php
@@ -9,21 +9,28 @@ $malfs = Malfunction::where('productsId', '=', $product->productsId)->get();
 
 @section('page-content')
 <div class="prod-cover">
-@foreach($malfs as $malf)
-    <div class="prod-content">
+    <div class="prod-content" style = "display: flex;
+    justify-content: center;">
     <div class="prod-text">
-       <div><h2 style="font-size: 40px">{{$malf->nome}}</h2></div>
+       <div><h2 style="font-size: 40px">MALFUNZIONAMENTI</h2></div>
     </div>
 </div>
 </div>
-<div class="prod-desc">
+
+@foreach($malfs as $malf)
+<div class="prod-text" style = "padding: 100px 300px;">
+<div><h4 style="font-size: 30px">{{$malf->nomeMalf}}</h4></div>
+</div>
+<div class="prod-desc" style = "margin-top: 20px;">
     <h3 style="margin:10px">PROBLEMA</h3>
-    <p style="margin:10px">{{$malf->descrizione}}</p>
+    <p style="margin:10px">{{$malf->problema}}</p>
 </div>
-<div class="prod-desc">
+<div class="prod-desc" style = "margin-top: 20px;">
     <h3 style="margin:10px">SOLUZIONI</h3>
-    <p style="margin:10px">{{$malf->noteTecniche}}</p>
+    <p style="margin:10px">{{$malf->soluzione}}</p>
 </div>
+<br>
+<br>
 
 @endforeach
 
