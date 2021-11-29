5d15a6e25f70e61777f72ed447e848741062bcdf
Alex Dediu <alex.dediu@outlook.it>
2021-11-29 17:11:40 +0100

    modifiche

--- laraProject/resources/views/pages/prodotto.blade.php
+++ laraProject/resources/views/pages/prodotto.blade.php
@@ -10,7 +10,7 @@
     @include('helpers/img-event', ['attrs' => 'imagefrm', 'imgFile' => $prodotto->imgName])
     </div>
     <div class="prod-text" style = "display:flex;">
-       <div><h2 style="font-size: 40px">{{$prodotto->nome}}</h2></div>
+       <div><h2 style="font-size: 25px">Nome Prodotto:<br>{{$prodotto->nome}}</h2></div>
        <br>
        <div><h5 style="font-size: 20px"> € {{$prodotto->prezzo}}</h5></div>
     </div>
@@ -25,8 +25,8 @@
     <p style="margin:10px">{{$prodotto->noteTecniche}}</p>
 </div>
 <div class="prod-desc">
-    <h3 style="margin:10px">GUIDA ALL'INSTALLAZIONE</h3>
-    <p style="margin:10px">{{$prodotto->modInstallaz}}</p>
+    <h3 style="margin:1vw">GUIDA ALL'INSTALLAZIONE</h3>
+    <p style="margin:1vw">{{$prodotto->modInstallaz}}</p>
 </div>
 @can('isStaff')
     
